7f055c5350a3ff3a2b12103683450e0360b48b13
vinyareddy39 <vinyareddy39@gmail.com>
2026-03-02 20:01:52 +0530

    week - 1 update

--- week1/bignumfun.js
+++ week1/bignumfun.js
@@ -1,14 +1,17 @@
 // Write a function that receives 3 number args and  return the big number
  
-function findMax(array)
+function findMax(num1, num2 , num3 )
 {
-    let max  = array[0]
-    for(let i = 0 ; i < array.length ; i++){
-    if (max < array[i]){
- max = array[i];
-    }
+ if (num1>num2 && num1>num3){
+    console.log("big number in three  ",num1)
 }
-return max ;
+else if(num2>num1 && num2>num3)
+{
+    console.log("big number in three  ",num2)
 }
-let result = findMax([10,20,30])// function call 
-console.log(result)
+else 
+{
+    console.log("big number in three  ",num3)
+}
+}
+ findMax(10,20,30)// function call 
